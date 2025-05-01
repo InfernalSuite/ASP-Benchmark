28a7936273abd0bf68d6aaab460aa2082be0b4a0
Vincenzo R. <vincenzoreinaa05@gmail.com>
2025-05-01 11:13:29 +0200

    Initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "9.0.0-beta13"
+}
+
+group = "com.infernalsuite"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+
+    maven {
+        name = "is-releases"
+        url = uri("https://repo.infernalsuite.com/repository/maven-releases/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("com.infernalsuite.asp:api:4.0.0-SNAPSHOT")
+}
+
+tasks.withType<ProcessResources> {
+    // Define the encoding for resource files
+    filteringCharset = "UTF-8"
+
+    filesMatching("plugin.yml") {
+        // Enable property expansion for plugin.yml
+        expand(project.properties) // Expands properties like 'version'
+    }
+}
+
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("ASP-Benchmark")
+    archiveClassifier.set("")
+    archiveVersion.set(rootProject.version.toString())
+}
---
+++ src/main/java/com/infernalsuite/benchmark/ASPBenchmark.java
@@ -0,0 +1,14 @@
+package com.infernalsuite.benchmark;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ASPBenchmark extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
